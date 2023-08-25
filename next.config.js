dea90b6c6dad9e6564eb5fd8cf7d2bd40e7d294b
ritaqyang <ritaqyang@gmail.com>
2023-08-25 11:59:55 -0400

    show info upon auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // 's.gravatar.com',
+    domains:['lh3.googleusercontent.com'], 
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig; 
